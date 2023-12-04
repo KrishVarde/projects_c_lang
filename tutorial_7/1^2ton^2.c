99c8c1e8d370c7ec531508e512c885f8a8936a00
armed_Senta <143574052+KrishVarde@users.noreply.github.com>
2023-12-04 15:56:06 +0000

    a

---
+++ tutorial_7/1^2ton^2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n=8,ans=0;
+    for (int i = 1; i <= n; i++)
+    {
+       ans=ans+i*i; 
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d^2+",i);
+    }
+    printf("\b%d",ans);
+    return 0;
+}
