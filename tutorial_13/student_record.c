61c25018758f5d93541e4a595f7d08f232be40b0
KrishVarde <143574052+KrishVarde@users.noreply.github.com>
2023-12-06 21:04:27 +0530

    a

--- tutorial_13/student_record.c
+++ tutorial_13/student_record.c
@@ -2,22 +2,23 @@
 #include <string.h>
 int main()
 {
+    int a[5],b;
     // declared stucture or union its your choice :)
     struct StudentRecord {
         char name[20];
         int en_num;
         int sub_marks[3];
         char grade[3];
-        int total[3];
+        int total;
     };
 
     // Array of struct instances
     struct StudentRecord stu[5] = {
-        {"chaganlal", 201229, {80, 91, 50}},
-        {"maganlal", 201230, {78, 95, 14}},
-        {"jethalal", 201231, {99, 99, 100}},
-        {"aiyar", 201232, {0, 12, 1}},
-        {"mangasingh", 201233, {70, 90, 80}}
+        {"chaganlal", 201229, {80, 91, 50},{0,0,0}},
+        {"maganlal", 201230, {78, 95, 14},{0,0,0}},
+        {"jethalal", 201231, {99, 99, 100},{0,0,0}},
+        {"aiyar", 201232, {0, 12, 1},{0,0,0}},
+        {"mangasingh", 201233, {70, 90, 80},{0,0,0}}
     };
 
     // dispaly grade its not choice its fixed :)
@@ -44,11 +45,29 @@ int main()
     {
         for (int j = 0; j < 3; j++)
         {
-            stu[i].total[i]=stu[i].sub_marks[j]+stu[i].total[i];
+            stu[i].total=stu[i].sub_marks[j]+stu[i].total;
+            a[i]=stu[i].total;
         }
         
     }
     
+    //declaring the topper by bubble shot
 
+    for (int i = 4; i >=0; i++)
+    {
+        if (a[i]>a[i+1])
+        {
+            int temp;
+            temp=a[i];
+            a[i]=a[i+1];
+            a[i+1]=temp;
+        }
+        if (a[0]==stu[i].total)
+        {
+            b=i;
+        }
+        
+    }
+    printf("\n\n   and the topper is......... %s",stu[b].name);
     return 0;
 }
