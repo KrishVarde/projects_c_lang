585e7669c0bcc94bc9ff5a81ca5d7d773e6dbb24
KrishVarde <143574052+KrishVarde@users.noreply.github.com>
2023-12-06 21:18:04 +0530

    ,

--- tutorial_13/student_record.c
+++ tutorial_13/student_record.c
@@ -2,10 +2,11 @@
 #include <string.h>
 int main()
 {
-    int a[5],b;
+    int a[5], b;
     // declared stucture or union its your choice :)
-    struct StudentRecord {
-        char name[20];
+    struct StudentRecord
+    {
+        char name[100];
         int en_num;
         int sub_marks[3];
         char grade[3];
@@ -14,12 +15,11 @@ int main()
 
     // Array of struct instances
     struct StudentRecord stu[5] = {
-        {"chaganlal", 201229, {80, 91, 50},{0,0,0}},
-        {"maganlal", 201230, {78, 95, 14},{0,0,0}},
-        {"jethalal", 201231, {99, 99, 100},{0,0,0}},
-        {"aiyar", 201232, {0, 12, 1},{0,0,0}},
-        {"mangasingh", 201233, {70, 90, 80},{0,0,0}}
-    };
+        {"chaganlal", 201229, {80, 91, 50}, {0, 0, 0}},
+        {"maganlal", 201230, {78, 95, 14}, {0, 0, 0}},
+        {"jethalal champaklal ghada", 201231, {99, 99, 100}, {0, 0, 0}},
+        {"aaiyar", 201232, {0, 12, 1}, {0, 0, 0}},
+        {"mangal singh", 201233, {70, 90, 80}, {0, 0, 0}}};
 
     // dispaly grade its not choice its fixed :)
     for (int i = 0; i < 5; i++)
@@ -28,46 +28,48 @@ int main()
         {
             if (stu[i].sub_marks[j] <= 100 && stu[i].sub_marks[j] >= 80)
             {
-                stu[i].grade[j]='A';
+                stu[i].grade[j] = 'A';
             }
             else if (stu[i].sub_marks[j] <= 79 && stu[i].sub_marks[j] >= 60)
-                stu[i].grade[j]='B';
+                stu[i].grade[j] = 'B';
             else if (stu[i].sub_marks[j] <= 59 && stu[i].sub_marks[j] >= 40)
-                stu[i].grade[j]='C';
+                stu[i].grade[j] = 'C';
             else if (stu[i].sub_marks[j] < 40)
-                stu[i].grade[j]='F';
+                stu[i].grade[j] = 'F';
         }
     }
 
-    //comparing and saying who has topped...
-    //we need to first make total
+    // comparing and saying who has topped...
+    // we need to first make total
     for (int i = 0; i < 5; i++)
     {
         for (int j = 0; j < 3; j++)
         {
-            stu[i].total=stu[i].sub_marks[j]+stu[i].total;
-            a[i]=stu[i].total;
+            stu[i].total = stu[i].sub_marks[j] + stu[i].total;
+            a[i] = stu[i].total;
         }
-        
     }
-    
-    //declaring the topper by bubble shot
 
-    for (int i = 4; i >=0; i++)
+    // declaring the topper by bubble shot
+
+    for (int i = 4; i > 0; i--)
     {
-        if (a[i]>a[i+1])
+        if (a[i] > a[i - 1])
         {
             int temp;
-            temp=a[i];
-            a[i]=a[i+1];
-            a[i+1]=temp;
+            temp = a[i];
+            a[i] = a[i - 1];
+            a[i - 1] = temp;
         }
-        if (a[0]==stu[i].total)
-        {
-            b=i;
-        }
-        
     }
-    printf("\n\n   and the topper is......... %s",stu[b].name);
+    for(int i=0;i<5;i++)
+    {
+        if (a[0] == stu[i].total)
+        {
+            b = i;
+            break;
+        }
+    }
+    printf("\n\nand the topper is......... %s with %d marks\n congratulations!", stu[b].name,stu[b].total);
     return 0;
 }
